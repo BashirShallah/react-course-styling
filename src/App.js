b8ff0b2a9ccb364d9909b178de43fa61b5d5442b
Bashir Shallah <bashir.shallah@gmail.com>
2018-12-26 18:58:27 +0300

    set style dynamically.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import './App.css';
 
 class App extends Component {
   state = {
-    className: 'green'
+    active: true
   }
 
   render() {
+    const style = {color:'white', 'font-weight': 'bold'};
+
+    if(this.state.active){
+      style.color = 'blue';
+    }
+
     return (
       <div>
-        <div className={this.state.className}>
+        <div className={this.state.active ? 'green': 'red'} style={style}>
           Test 
         </div>
-        <button onClick={()=>{this.setState({className: 'red'})}}>
-          change to red
-        </button>
-        <button onClick={()=>{this.setState({className: 'green'})}}>
-          change to green
+
+        <button onClick={()=>{this.setState({active: ! this.state.active})}}>
+          change active
         </button>
       </div>
     );
